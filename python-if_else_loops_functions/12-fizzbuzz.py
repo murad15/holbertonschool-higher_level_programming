302e269b4429043a539079e96b75ca7ae990acb5
murad15 <muradis1503@gmail.com>
2025-12-01 15:21:29 +0000

    q

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-def add(a, b):
-   return a+b
+def fizzbuzz():
+    for i in range(101):
+        if (i % 3 == 0) & (i % 5 == 0):
+           print("FizzBuzz", end = " ")
+	elif i % 3 == 0:
+	   print("Fizz", end = " ")
+  	elif i % 5 == 0:
+	   print("Buzz", end = " ")
+	else:
+	   print(i, end = " ")

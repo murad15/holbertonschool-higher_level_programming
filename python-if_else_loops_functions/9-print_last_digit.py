cd64866fb2d3ca085931b16f204e32a6ee5ede35
murad15 <muradis1503@gmail.com>
2025-12-01 14:32:04 +0000

    tt

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    try:
-        last = abs(int(number)) % 10
-        print(last)
-        return last
-    except (ValueError, TypeError):
-        print("Input must be a number")
-        return None
+    last = abs(int(number)) % 10
+    print(last)
+    return last

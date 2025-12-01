c5768906ca3ca181864c997c5c2a60fe3d2011c9
murad15 <muradis1503@gmail.com>
2025-12-01 07:31:01 +0000

    ta

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def islower(c):
-    return ord('A') <= ord(c) <= ord('Z')
+def uppercase(s):
+    res = ""
+    for c in s:
+        if 'a' <= c <= 'z':
+            res += chr(ord(c) - 32)
+        else:
+            res += c
+    print("{}".format(res))

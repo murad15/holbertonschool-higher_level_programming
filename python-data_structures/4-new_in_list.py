896300b961b5ad0c3a065c6814b68486e4757c63
murad15 <muradis1503@gmail.com>
2025-12-02 20:47:59 +0000

    q

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        b = my_list.copy()
+        return b
+    else:
+        b = my_list.copy()
+        b[idx] = element
+        return b

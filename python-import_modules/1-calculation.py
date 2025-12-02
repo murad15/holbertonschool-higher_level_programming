13f7f7acfa8959e8b6644cf31a8bcbaabfa330d1
murad15 <muradis1503@gmail.com>
2025-12-02 15:09:32 +0000

    q

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+if __name__ == "__main__":
+    "{} + {} = {}".format(a, b, add(a, b))
+    "{} - {} = {}".format(a, b, sub(a, b))
+    "{} * {} = {}".format(a, b, mul(a, b))
+    "{} / {} = {}".format(a, b, fiv(a, b))
+
+
+

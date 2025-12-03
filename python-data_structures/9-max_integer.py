393dc2b13a0d88d46b2ecdc4dac5586b63746d0d
murad15 <muradis1503@gmail.com>
2025-12-03 09:36:31 +0000

    q

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    m = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in list:
+            if i > m:
+                m = i
+    return m

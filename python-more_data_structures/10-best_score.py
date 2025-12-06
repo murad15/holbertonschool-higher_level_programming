61db587c50204cf7de7e7f459a900a4cfff0cb87
murad15 <muradis1503@gmail.com>
2025-12-06 11:40:33 +0000

    q

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    elif len(a_dictionary) < 1:
-        return None
-    else:
-        new_dict = {k: v for k, v in sorted(a_dictionary.items(), key = lambda item: item[1], reverse = True)}
-        best_key = list(new_dict.keys())[0]
-        return best_key
+    return max(a_dictionary, key=a_dictionary.get)

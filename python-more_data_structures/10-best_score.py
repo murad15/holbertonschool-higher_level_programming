ffb455503d316c5ae738bcb28e5a06772d78d08a
murad15 <muradis1503@gmail.com>
2025-12-06 11:32:35 +0000

    q

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) < 1:
+    if len(a_dictionary) is None:
         return None
     else:
         new_dict = {k: v for k, v in sorted(a_dictionary.items(), key = lambda item: item[1], reverse = True)}

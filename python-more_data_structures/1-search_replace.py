69fbde51b1be7cc347647a0ff41e5def1fb1c346
murad15 <muradis1503@gmail.com>
2025-12-06 08:27:00 +0000

    q

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in range(len(my_list)):
         if my_list[i] == search:
-            my_list[i] = replace
+            new_list[i] = replace
         else:
             continue
     return new_list

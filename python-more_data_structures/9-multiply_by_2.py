32ca5ce02fbc0fe799041fe7dd1ab04391bff750
murad15 <muradis1503@gmail.com>
2025-12-06 10:45:52 +0000

    q

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary.keys():
-        a_dictionary[i] = a_dictionary[i]*2
-    return a_dictionary
+    b_dict = a_dictionary.copy()
+    for i in b_dict.keys():
+        b_dict[i] = b_dict[i]*2
+    return b_dict

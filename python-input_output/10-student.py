7f21ad5e493360b1dc789ac3feae063c125f3c7f
murad15 <muradis1503@gmail.com>
2025-12-14 19:34:18 +0000

    gitpusher

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student class definition with selective JSON serialization"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student with first_name, last_name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return the dictionary representation of a Student instance.
+        If attrs is a list of strings, only include those attributes.
+        Otherwise, include all attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__.copy()

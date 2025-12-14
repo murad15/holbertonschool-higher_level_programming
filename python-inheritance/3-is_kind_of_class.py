d2878c7d081f8f5d482e480a3a0c806335d61556
murad15 <muradis1503@gmail.com>
2025-12-14 18:27:47 +0000

    gitpusher

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,13 +3,6 @@
 
 
 def is_same_class(obj, a_class):
-    """Check if `obj` is exactly an instance of `a_class`.
+    """Check if `obj` is exactly an instance of the class a_class"""
 
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to compare against.
-
-    Returns:
-        bool: True i, False otherwise.
-    """
     return type(obj) is a_class

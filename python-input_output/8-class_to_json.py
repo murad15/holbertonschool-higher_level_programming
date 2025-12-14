e626b41abaf9d01240bfb73a010f071f5cdf5e74
murad15 <muradis1503@gmail.com>
2025-12-14 19:32:37 +0000

    gitpusher

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns a dictionary description for JSON serialization of an object"""
+
+
+def class_to_json(obj):
+    """Return the dictionary epresentation of obj's attributes"""
+    return obj.__dict__.copy()

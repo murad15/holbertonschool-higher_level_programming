1f2890a2c2d255ab69149ac8138f9ac538dfe943
murad15 <muradis1503@gmail.com>
2025-12-14 19:27:46 +0000

    gitpusher

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Function that writes a string to a text file and returns the number of characters written"""
+"""Function that writes a string returns the number of characters written"""
 
 
 def write_file(filename="", text=""):
-    """Writes text to a UTF-8 file and returns the number of characters written"""
+    """Writes text to a UTF-8 and returns the number of characters written"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)

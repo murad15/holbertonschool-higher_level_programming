27bc931afb5a8f0016bddcc2175c6ed4fe7fbbab
murad15 <muradis1503@gmail.com>
2025-12-14 17:42:24 +0000

    gitpusher

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Inherit from list"""
+
+
+class MyList(list):
+    """Inheritance from list"""
+
+
+    def print_sorted(self):
+        """Print something here"""
+        print(sorted(self))

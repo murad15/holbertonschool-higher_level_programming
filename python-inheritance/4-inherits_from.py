954cee78bf1a1fd54a8a07e445501d499202ab09
murad15 <muradis1503@gmail.com>
2025-12-14 18:33:39 +0000

    gitpusher

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Return True if obj is an instance of a class its from a_class."""
-    return type(obj) != a_class and isinstance(obj, a_class)
+    return type(obj) is not a_class and isinstance(obj, a_class)

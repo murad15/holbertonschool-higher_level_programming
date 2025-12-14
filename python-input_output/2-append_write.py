66551b6d59955448fb477e152a1e0c7627bdd31e
murad15 <muradis1503@gmail.com>
2025-12-14 19:37:38 +0000

    gitpusher

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Function that appends a string and returns the number of characters added"""
+"""Function that appends a str and returns the number of characters added"""
 
 
 def append_write(filename="", text=""):
-    """Appends text to a UTF-8 file and returns the number of characters added"""
+    """Appends text to a -8 file and returns the number of characters added"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)

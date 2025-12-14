56b4de7744e4c8ebb2bfc569ab9810e63bd5dfcc
murad15 <muradis1503@gmail.com>
2025-12-14 18:32:07 +0000

    gitpusher

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Check if an object is an instance of a specified class."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class its from a_class."""
+    return type(obj) != a_class and isinstance(obj, a_class)

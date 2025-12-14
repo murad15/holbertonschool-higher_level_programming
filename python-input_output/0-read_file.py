b9ac436029d28eb4cf09b03c9dc3afb7e8db727a
murad15 <muradis1503@gmail.com>
2025-12-14 19:14:33 +0000

    gitpusher

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its content to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end

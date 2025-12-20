234c38ce0a5962c709a69dbe99953f16ffaf6b2d
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2025-12-21 01:57:37 +0400

    Create task_02_csv.py

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import pickle
+import csv 
+import json
+
+def convert_csv_to_json(filename):
+    try:
+        with open(filename, mode='r', newline='', encoding='utf-8') as csvfile:
+             data = list(csv.DictReader(csvfile))
+    
+        with open('data.json', mode='w', encoding='utf-8') as jsonfile:
+             json.dump(data, jsonfile)
+    
+        return True
+
+    except (FileNotFoundError, IOError):
+        return False

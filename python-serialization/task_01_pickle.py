dd2a3159eb2f9a2a315c217efcf19344ba251265
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2025-12-21 01:36:36 +0400

    Update task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -13,12 +13,18 @@ class CustomObject:
         print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
 
     def serialize(self, filename):
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except (FileNotFoundError, PermissionError, pickle.UnpicklingError, EOFError):
+            return None
 
     @classmethod
     def deserialize(cls, filename):
-        with open(filename, "r") as f:
-            return pickle.load(f)
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except (FileNotFoundError, PermissionError, pickle.UnpicklingError, EOFError):
+            return None
 
     

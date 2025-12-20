ddf0f4c898371d6460307ca46906a1393ca0e60d
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2025-12-21 01:32:33 +0400

    Create task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import json
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+      
+    def display(self):
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "r") as f:
+            return pickle.load(f)
+
+    

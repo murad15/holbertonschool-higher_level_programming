c57cadac81f633844a4fe989bc44510f439ad3bd
murad15 <muradis1503@gmail.com>
2025-12-21 09:53:46 +0000

    gitpusher

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,15 +1,4 @@
-#!/bin/bash
--- Comment to start
+-- Comment to start the taks
 
 
-
-# Check if a database name was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
-
-DB_NAME=$1
-
-# Execute the command
-mysql -u your_username -p -e "SHOW TABLES;" "$DB_NAME"
+SHOW TABLES;

d5c2a147c4de13250c513517ff3ff618367a5409
murad15 <muradis1503@gmail.com>
2025-12-21 16:00:23 +0000

    gitpusher

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,6 @@
 --- dasd as d asadsa s
 
 
-
--- Lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id

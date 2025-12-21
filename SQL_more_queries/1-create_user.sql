6de8e92011453c72e1332ab980993ee8d5596309
murad15 <muradis1503@gmail.com>
2025-12-21 13:48:58 +0000

    gitpusher

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- Comme ntn asd  asd
 
 
-CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;

e032a8fe41aa23f8885f18dc6d225b7c8a21b71e
murad15 <muradis1503@gmail.com>
2025-12-21 13:29:08 +0000

    gitpusher

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- COMMENTS FFKIAF FJAF FJFA
 
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

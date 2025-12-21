785de02e61c2151c1c43d1d9afcb4a0134a87797
murad15 <muradis1503@gmail.com>
2025-12-21 15:30:03 +0000

    gitpusher

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- asdm askdas ad a d
+
+
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;

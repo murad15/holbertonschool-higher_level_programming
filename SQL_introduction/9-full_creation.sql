561132a294b8ffbd5890b6b37d451f301b84a00a
murad15 <muradis1503@gmail.com>
2025-12-21 11:51:51 +0000

    gitpusher

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- COMMENT COafn dsjnaslda snasdk
 
 
-CREATE TABLE IF EXISTS second_table
+CREATE TABLE IF NOT EXISTS second_table
 (
-   id INT PRIMARY KEY,
+   id INT,
    name VARCHAR(256),
    score INT
 );
